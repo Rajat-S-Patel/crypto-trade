6b32d160058c7e7bccabbf3bccfafea14980fb21
Rajat Patel <18bce191@nirmauni.ac.in>
2021-12-31 12:15:38 +0530

    Initial Commit

---
+++ src/main/java/com/pirimid/cryptotrade/CryptoTradeApplication.java
@@ -0,0 +1,13 @@
+package com.pirimid.cryptotrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoTradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoTradeApplication.class, args);
+    }
+
+}
