f83583c8b5a653b7d49e2e428c387ac4de06fc34
Rajat Patel <18bce191@nirmauni.ac.in>
2022-01-11 17:14:32 +0530

    Added Order related functionalities for Gemini

--- src/main/java/com/pirimid/cryptotrade/util/ExchangeUtil.java
+++ src/main/java/com/pirimid/cryptotrade/util/ExchangeUtil.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Component;
 public class ExchangeUtil {
     @Autowired
     public ExcCoinbase exc_coinbase;
-
     @Autowired
     public ExcGemini excGemini;
 
