a80d2c38fd0fc81d0c0b954e461708b829aa1b2d
vedant1551 <vedantrajyaguru.333@gmail.com>
2022-02-01 11:41:03 +0530

    removed unnecessory imports

--- src/main/java/com/pirimid/cryptotrade/CryptoTradeApplication.java
+++ src/main/java/com/pirimid/cryptotrade/CryptoTradeApplication.java
@@ -1,15 +1,7 @@
 package com.pirimid.cryptotrade;
 
-import com.pirimid.cryptotrade.model.User;
-import com.pirimid.cryptotrade.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.sql.Date;
-import java.time.Instant;
-import java.util.Optional;
 
 @SpringBootApplication
 public class CryptoTradeApplication {
