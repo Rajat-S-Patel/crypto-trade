5c3a7188e1e112ef0b23262ced5997ab2f31b38c
vedantr1551 <vedant.rajyaguru@pirimidtech.com>
2022-04-28 16:39:13 +0530

    Added account id for getExchanges

--- src/main/java/com/pirimid/cryptotrade/controller/ExchangeController.java
+++ src/main/java/com/pirimid/cryptotrade/controller/ExchangeController.java
@@ -1,5 +1,6 @@
 package com.pirimid.cryptotrade.controller;
 
+import com.pirimid.cryptotrade.DTO.ExchangeDto;
 import com.pirimid.cryptotrade.DTO.SymbolResDTO;
 import com.pirimid.cryptotrade.model.Exchange;
 import com.pirimid.cryptotrade.service.ExchangeService;
@@ -7,12 +8,11 @@ import com.pirimid.cryptotrade.util.ExchangeUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
+
 @CrossOrigin
 @RestController
 public class ExchangeController {
@@ -23,6 +23,13 @@ public class ExchangeController {
     @GetMapping("/exchanges")
     public ResponseEntity<?> getAllExchanges(){
         List<Exchange> exchangeList = exchangeService.getAllExchanges();
+        if(exchangeList==null)
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(exchangeList);
+    }
+    @GetMapping("/exchanges/{userid}")
+    public ResponseEntity<?> getAllExchangesByUserid(@PathVariable(required = true) UUID userid){
+        List<ExchangeDto> exchangeList = exchangeService.getAllExchanges(userid);
         if(exchangeList==null)
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(exchangeList);
--- src/main/java/com/pirimid/cryptotrade/service/ExchangeService.java
+++ src/main/java/com/pirimid/cryptotrade/service/ExchangeService.java
@@ -1,11 +1,16 @@
 package com.pirimid.cryptotrade.service;
 
+import com.pirimid.cryptotrade.DTO.ExchangeDto;
 import com.pirimid.cryptotrade.DTO.SymbolResDTO;
 import com.pirimid.cryptotrade.model.Exchange;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface ExchangeService {
+
+    List<ExchangeDto> getAllExchanges(UUID userid);
+
     List<Exchange> getAllExchanges();
     List<SymbolResDTO> getPairs(String exchangeName);
     void fetchAllPairs();
